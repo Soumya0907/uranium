3dde66b00176faed6a2f7745452fc98096319d0c
Soumya0907 <soumyajha32@gmail.com>
2022-04-11 21:26:38 +0530

    mongoDb

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose} = require('mongoose');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://Soumya0907:soumya09@cluster0.j4lvv.mongodb.net/Soumya0907?retryWrites=true&w=majority",{
+    useNewUrlParser:true
+
+})
+.then(()=>console.log("MongoDb is connected"))
+.catch(err=>console.log(err))
 
 app.use('/', route);
 
